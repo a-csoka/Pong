018313411e1fa6ec7fbba3c86526afdc717f767c
Csóka Csaba <acsokacsaba@gmail.com>
2022-09-25 15:52:30 +0200

    Fixed StartPage

--- src/components/StartPage.jsx
+++ src/components/StartPage.jsx
@@ -2,21 +2,14 @@ import React, { Component } from 'react';
 import "./css/StartPage.css";
 
 class StartPage extends Component {
-    state = {  } 
-    constructor(props) {
-        super(props)
-    }
-
     render() { 
         return (
         <React.Fragment>
             <h1 className='gameTitle'>PONG</h1>
             <h2 className="selectGameModeText">Válassz játékmódot!</h2>
             <div className="gamemodesWrapper">
-                <button id="TwoPlayerBtn" onClick= { () => this.props.startGameHandler("TwoPlayer")}>2 Játékos</button>
-                <button id="EasyBtn">Könnyű</button>
-                <button id="MediumBtn">Közepes</button>
-                <button id="HardBtn">Nehéz</button>
+                <button id="EasyBtn" onClick= { () => this.props.startGameHandler("Easy")}>1 Játékos</button>
+                <button id="TwoPlayersBtn" onClick= { () => this.props.startGameHandler("TwoPlayer")}>2 Játékos</button>
             </div>
         </React.Fragment>);
     }
